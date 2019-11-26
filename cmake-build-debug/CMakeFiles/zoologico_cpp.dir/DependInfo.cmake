4c361a1e207b3e9142540506a543c9a97fe342f1
eullersaez <53879075+eullersaez@users.noreply.github.com>
2019-11-26 19:37:19 -0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/zoologico_cpp.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ana_carolina/Downloads/zoologico-cpp/Interface/AbstractInterface.cpp" "/home/ana_carolina/Downloads/zoologico-cpp/cmake-build-debug/CMakeFiles/zoologico_cpp.dir/Interface/AbstractInterface.cpp.o"
+  "/home/ana_carolina/Downloads/zoologico-cpp/Interface/BuscaAnimal.cpp" "/home/ana_carolina/Downloads/zoologico-cpp/cmake-build-debug/CMakeFiles/zoologico_cpp.dir/Interface/BuscaAnimal.cpp.o"
+  "/home/ana_carolina/Downloads/zoologico-cpp/Interface/CadastroAnimal.cpp" "/home/ana_carolina/Downloads/zoologico-cpp/cmake-build-debug/CMakeFiles/zoologico_cpp.dir/Interface/CadastroAnimal.cpp.o"
+  "/home/ana_carolina/Downloads/zoologico-cpp/Interface/Dashboard.cpp" "/home/ana_carolina/Downloads/zoologico-cpp/cmake-build-debug/CMakeFiles/zoologico_cpp.dir/Interface/Dashboard.cpp.o"
+  "/home/ana_carolina/Downloads/zoologico-cpp/Interface/Perfil.cpp" "/home/ana_carolina/Downloads/zoologico-cpp/cmake-build-debug/CMakeFiles/zoologico_cpp.dir/Interface/Perfil.cpp.o"
+  "/home/ana_carolina/Downloads/zoologico-cpp/TAD/AbstractConnection.cpp" "/home/ana_carolina/Downloads/zoologico-cpp/cmake-build-debug/CMakeFiles/zoologico_cpp.dir/TAD/AbstractConnection.cpp.o"
+  "/home/ana_carolina/Downloads/zoologico-cpp/TAD/Ambiente.cpp" "/home/ana_carolina/Downloads/zoologico-cpp/cmake-build-debug/CMakeFiles/zoologico_cpp.dir/TAD/Ambiente.cpp.o"
+  "/home/ana_carolina/Downloads/zoologico-cpp/TAD/Animal.cpp" "/home/ana_carolina/Downloads/zoologico-cpp/cmake-build-debug/CMakeFiles/zoologico_cpp.dir/TAD/Animal.cpp.o"
+  "/home/ana_carolina/Downloads/zoologico-cpp/TAD/HistoricoMedico.cpp" "/home/ana_carolina/Downloads/zoologico-cpp/cmake-build-debug/CMakeFiles/zoologico_cpp.dir/TAD/HistoricoMedico.cpp.o"
+  "/home/ana_carolina/Downloads/zoologico-cpp/main.cpp" "/home/ana_carolina/Downloads/zoologico-cpp/cmake-build-debug/CMakeFiles/zoologico_cpp.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/zoologico_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zoologico_cpp.dir/Interface/AbstractInterface.cpp.o"
+  "CMakeFiles/zoologico_cpp.dir/Interface/BuscaAnimal.cpp.o"
+  "CMakeFiles/zoologico_cpp.dir/Interface/CadastroAnimal.cpp.o"
+  "CMakeFiles/zoologico_cpp.dir/Interface/Dashboard.cpp.o"
+  "CMakeFiles/zoologico_cpp.dir/Interface/Perfil.cpp.o"
+  "CMakeFiles/zoologico_cpp.dir/TAD/AbstractConnection.cpp.o"
+  "CMakeFiles/zoologico_cpp.dir/TAD/Ambiente.cpp.o"
+  "CMakeFiles/zoologico_cpp.dir/TAD/Animal.cpp.o"
+  "CMakeFiles/zoologico_cpp.dir/TAD/HistoricoMedico.cpp.o"
+  "CMakeFiles/zoologico_cpp.dir/main.cpp.o"
+  "zoologico_cpp"
+  "zoologico_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zoologico_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
